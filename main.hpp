5039739fa9ed591fd05bf34cf6eed318ac0a0d26
Sricharan Vagvala <charansv19@gmail.com>
2026-05-11 14:06:26 -0700

    completed the code.

--- main.hpp
+++ main.hpp
@@ -11,15 +11,45 @@ void insertone(int[], int &, int);
 void deleteone(int[], int &, int);
 void printout(int[], int);
 
-void insertone(int number[], int &length, int usernum)
+void insertone(int numbers[], int &N, int value)
 {
-    // TODO: insert usernum into the sorted array, then increment length
+    // Find insertion position
+    int pos = 0;
+    while (pos < N && numbers[pos] < value) {
+        pos++;
+    }
+
+    // Shift elements right to make space
+    for (int i = N; i > pos; i--) {
+        numbers[i] = numbers[i - 1];
+    }
+
+    // Insert value and update size
+    numbers[pos] = value;
+    N++;
 }
 
-void deleteone(int number[], int &length, int usernum)
+
+void deleteone(int numbers[], int &N, int value)
 {
-    // TODO: find usernum, shift remaining elements left, decrement length
+    // Find one instance of value
+    int pos = -1;
+    for (int i = 0; i < N; i++) {
+        if (numbers[i] == value) {
+            pos = i;
+            break;
+        }
+    }
+
+    // If found, shift left to delete it
+    if (pos != -1) {
+        for (int i = pos; i < N - 1; i++) {
+            numbers[i] = numbers[i + 1];
+        }
+        N--;
+    }
 }
+
 
 void printout(int number[], int last)
 {
